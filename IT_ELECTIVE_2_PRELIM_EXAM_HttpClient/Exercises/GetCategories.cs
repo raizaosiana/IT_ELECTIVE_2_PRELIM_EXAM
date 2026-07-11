6ad5f1dea0c53c20750b24497998d5ea4956896d
raizaosiana <raizaosiana28@gmail.com>
2026-07-11 08:53:18 +0800

    Update GetCategories.cs
    Signed-off-by: raizaosiana <raizaosiana28@gmail.com>

--- IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/GetCategories.cs
+++ IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/GetCategories.cs
@@ -1,3 +1,6 @@
+using System.Net;
+using System.Text.Json;
+
 namespace IT_ELECTIVE_2_PRELIM_EXAM_HttpClient.Exercises;
 
 // EXERCISE 4: GET List Categories
@@ -19,6 +22,22 @@ public static class GetCategories
         // TODO: Parse the response JSON
         // TODO: Assert the "categories" array has more than 0 items
 
-        throw new NotImplementedException();
+        // Send GET request
+        var response = await client.GetAsync("https://themealdb.com/api/json/v1/1/categories.php");
+
+        // Assert status code is 200 OK
+        if (response.StatusCode != HttpStatusCode.OK)
+            throw new Exception("Expected status code 200 OK.");
+
+        // Read and parse the response JSON
+        var body = await response.Content.ReadAsStringAsync();
+        using var document = JsonDocument.Parse(body);
+
+        // Get the categories array
+        var categories = document.RootElement.GetProperty("categories");
+
+        // Assert the array has more than 0 items
+        if (categories.ValueKind == JsonValueKind.Null || categories.GetArrayLength() == 0)
+            throw new Exception("No categories found.");
     }
 }
